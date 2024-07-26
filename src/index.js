67fe1f79f2fae5aabb9b8bc3292883f5e6d1724a
[BladrenS] <denis_ka_98@mail.ru>
2024-07-26 21:23:51 +0600

    fix: delete extra comments

--- src/index.js
+++ src/index.js
@@ -25,18 +25,3 @@ module.exports = function check(str, bracketsConfig) {
    }
   return stack.length === 0;
 }
-
-
-/*const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
-
-console.log(check('((()))()', config1),
-check('|()|', config5),
-check('|()|(||)||', config5));*/
-
-
